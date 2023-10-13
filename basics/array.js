730937caa34e1d5ff5f2902236a55e785aa19bcf
AsutoshBiswal007 <97107828+AsutoshBiswal007@users.noreply.github.com>
2023-10-13 06:13:48 +0000

    arraybasics

---
+++ basics/array.js
@@ -0,0 +1,12 @@
+// let myArray= [0 , 1 , 2 , 3 , 4 , 5];
+// // console.log(myArray[1]);
+// // myArray.push(6)
+// // myArray.pop();
+// let newArray = myArray.join()
+// console.log(myArray);
+// console.log(newArray);
+
+const marvel_heros = ["thor", "ironman", "spiderman"];
+const dc_heros = ["batman", "flash", "superman"];
+console.log(marvel_heros.concat (dc_heros));
+
